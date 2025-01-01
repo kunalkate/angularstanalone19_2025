c5b8ac081eba1d57e46a649c0be3e4102a6ebd1c
an-kunalkate <kunal.k@applicationnexus.com>
2025-01-01 23:48:24 +0530

    initial commit

---
+++ angular_19_standalone/src/app/pages/parent-to-child/parent-to-child.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-to-child',
+  imports: [],
+  templateUrl: './parent-to-child.component.html',
+  styleUrl: './parent-to-child.component.scss'
+})
+export class ParentToChildComponent {
+
+}
