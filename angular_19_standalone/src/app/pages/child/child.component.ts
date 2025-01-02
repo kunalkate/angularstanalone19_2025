f6ad31c7a6f3c78f278d558703b0bf440fe48095
an-kunalkate <kunal.k@applicationnexus.com>
2025-01-02 10:25:23 +0530

    Added the parent to child data sharing

--- angular_19_standalone/src/app/pages/child/child.component.ts
+++ angular_19_standalone/src/app/pages/child/child.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss'
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit  {
+@Input() childData: any;
 
+ngOnInit(){
+  console.log("Child Component Data: ", this.childData);  // This will log the data received from parent component.
+}
 }
